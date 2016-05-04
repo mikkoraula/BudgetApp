a7022364361770b96ba4cca20c2b235aa2c564a4
meidec <mikko.raula@gmail.com>
2016-05-04 12:47:12 +0300

    initial commit with basic stuff

---
+++ app/src/main/java/com/example/mikko/budgetapplication/BackendSettings.java
@@ -0,0 +1,11 @@
+package com.example.mikko.budgetapplication;
+
+/**
+ * Provides values used to communicate with backend.
+ */
+public class BackendSettings
+{
+    public final static String ANDROID_SECRET_KEY = "F5822A7A-12D8-6BC2-FF0E-305690534900";
+    public final static String APPLICATION_ID = "26C0783B-8F0E-790D-FFBB-68DCAEFF9200";
+    public static final String VERSION = "v1";
+}
---
+++ app/src/main/java/com/example/mikko/budgetapplication/LoadingCallback.java
@@ -0,0 +1,73 @@
+package com.example.mikko.budgetapplication;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import com.backendless.async.callback.AsyncCallback;
+import com.backendless.exceptions.BackendlessFault;
+
+/**
+ * A callback, which has ability to show loading dialog while response is being received.
+ * Shows Toast with result's toString() on success.
+ * Shows AlertDialog with error message on failure.
+ * If overriding handleResponse and/or handleFault, should manually dismiss ProgressDialog with hideLoading() method
+ * or calling super.handleResponse() or super.handleFault().
+ *
+ * @param <T> class to be received from server
+ */
+public class LoadingCallback<T> implements AsyncCallback<T>
+{
+    private Context context;
+    private ProgressDialog progressDialog;
+
+    /**
+     * Create an instance with message "Loading...".
+     *
+     * @param context context to which ProgressDialog should be attached
+     */
+    public LoadingCallback( Context context )
+    {
+        this( context, context.getString( R.string.loading_empty ) );
+    }
+
+    /**
+     * Creates an instance with given message.
+     *
+     * @param context        context to which ProgressDialog should be attached
+     * @param loadingMessage message to be shown on ProgressDialog
+     */
+    public LoadingCallback( Context context, String loadingMessage )
+    {
+        this.context = context;
+        progressDialog = new ProgressDialog( context );
+        progressDialog.setMessage( loadingMessage );
+    }
+
+    @Override
+    public void handleResponse( T response )
+    {
+        progressDialog.dismiss();
+    }
+
+    @Override
+    public void handleFault( BackendlessFault fault )
+    {
+        progressDialog.dismiss();
+        DialogHelper.createErrorDialog( context, "BackendlessFault", fault.getMessage() ).show();
+    }
+
+    /**
+     * Shows ProgressDialog.
+     */
+    public void showLoading()
+    {
+        progressDialog.show();
+    }
+
+    /**
+     * Hides ProgressDialog.
+     */
+    public void hideLoading()
+    {
+        progressDialog.dismiss();
+    }
+}
